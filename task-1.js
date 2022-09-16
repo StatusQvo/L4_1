76b45a4d6658e190035a9cd509100ad43d0a14f9
Darya_Savitskaya <statusqvogerz@gmail.com>
2022-09-17 01:35:33 +0500

    L4T1

---
+++ task-1.js
@@ -0,0 +1,17 @@
+let per1 = 37
+let per2 = 'JavaScript'
+let per3 = 51n
+let per4 = null
+let per5 = undefined
+let per6 = { mynum: 23 }
+let per7 = false
+let per8 = Symbol('mysimb')
+
+console.log(Number(per1), String(per1), Boolean(per1))
+console.log(Number(per2), String(per2), Boolean(per2))
+console.log(Number(per3), String(per3), Boolean(per3))
+console.log(Number(per4), String(per4), Boolean(per4))
+console.log(Number(per5), String(per5), Boolean(per5))
+console.log(Number(per6), String(per6), Boolean(per6))
+console.log(Number(per7), String(per7), Boolean(per7))
+console.log(String(per8), Boolean(per8))
